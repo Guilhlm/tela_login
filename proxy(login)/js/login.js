c72042072799636554b20f01c07888d046fd6c0b
Guilherme Maia <141831938+Guilhlm@users.noreply.github.com>
2024-05-16 11:40:59 -0300

    cs

---
+++ proxy(login)/js/login.js
@@ -0,0 +1,21 @@
+function validar()
+{
+
+    var user = document.getElementById("texto").value;
+    var pass = document.getElementById("senha").value;
+
+
+    if (user === "Picapau" && pass === "Csgo_7070!") {
+
+        alert("deu certo garai ");
+
+        window.location.href = "https://www.sp.senac.br/";
+    }
+    else{
+        
+        alert("dfdsjfuagfgsfdg");
+
+        window.location.href = "index.html";
+    }
+
+}   
